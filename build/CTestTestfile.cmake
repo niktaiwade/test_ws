e58024cabbbb63e785d6b4b672c9ece05ded434d
niktaiwade <niktaiwade@gmail.com>
2021-05-26 11:51:17 +0530

    Add existing file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashta/test_ws/src
+# Build directory: /home/ashta/test_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("franka_moveit_config")
+subdirs("franka_description_gazebo")
---
+++ build/franka_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashta/test_ws/src/franka_moveit_config
+# Build directory: /home/ashta/test_ws/build/franka_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
